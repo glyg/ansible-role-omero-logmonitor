f0bd776d2c0307ca8eeeb98fd120cb46115e853f
Simon Li <spli@dundee.ac.uk>
2020-03-09 17:03:07 +0000

    Molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,16 +1,17 @@
+import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_service_running_and_enabled(Service):
-    service = Service('omero-logmonitor')
+def test_service_running_and_enabled(host):
+    service = host.service('omero-logmonitor')
     assert service.is_running
     assert service.is_enabled
 
 
-def test_config_file(File):
-    f = File('/opt/omero/logmonitor/omero-fenton/omero-fenton.cfg')
+def test_config_file(host):
+    f = host.file('/opt/omero/logmonitor/omero-fenton/omero-fenton.cfg')
     assert f.contains('/opt/omero/server/OMERO.server/var/log/Blitz-0.log')
     assert f.contains('/opt/omero/web/OMERO.web/var/log/OMEROweb.log')
